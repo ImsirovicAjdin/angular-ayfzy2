dd886846e31483b97b9fe5ccc6f0bf9e617af825
ImsirovicAjdin <ajdika@live.com>
2019-07-18 11:18:57 +0200

    (1) Add AppComponentClass.vehicles, (2) add *ngFor to <app-child>, (3) assign 'vehicle' to [car] binding, (4) @Input() car into AppChildComponent.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,14 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <div>Hide-show with ngSwitch</div>
-    <app-child></app-child>
+    <app-child *ngFor="let vehicle of vehicles" [car]="vehicle"></app-child>
   `
 })
 export class AppComponent  {
-
+  vehicles = [
+    { id: 1, make: 'Toyota', color: { body: 'red', interior: 'gray' } },
+    { id: 2, make: 'Subaru', color: { body: 'red', interior: 'gray' } },
+    { id: 3, make: 'VW', old: 'yes' },
+    { id: 4, make: 'Other', color: { body: 'yellow', interior: 'gray' } },
+  ]
 }
