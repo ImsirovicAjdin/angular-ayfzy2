8acc5ea9d63b11419826553b9b1d90fbdcf04c55
ImsirovicAjdin <ajdika@live.com>
2019-07-18 11:12:30 +0200

    (1) Remove hello, (2) Add inline HTML template to AppComponent class, (3) remove AppComponent's .css and .html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  template: `
+    <div>Hide-show with ngSwitch</div>
+  `
 })
 export class AppComponent  {
-  name = 'Angular';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
+// Hide-show with ngSwitch
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
