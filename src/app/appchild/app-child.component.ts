2190ea959441d4b7302d5a39fd832efeb743ef4a
ImsirovicAjdin <ajdika@live.com>
2019-07-18 11:23:51 +0200

    Only showing if car is old

--- src/app/appchild/app-child.component.ts
+++ src/app/appchild/app-child.component.ts
@@ -5,6 +5,11 @@ import { Component, Input, OnInit } from '@angular/core';
   template: `
     <div style="display: inline-block; overflow: auto; height: 200px; width: 240px; background: plum; color: white; margin: 10px; padding: 20px;">
       Child component
+      <div>ID: {{car?.id}}</div>
+      <div [hidden]="!car?.old">
+        <div>Only showing if car is old!</div>
+        <div>CAR MAKE: {{car?.make}}</div>
+      </div>
     </div>
   `
 })
